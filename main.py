9d6fc27b4bb71ff23d31f4769217b4d52dbbce6b
Albert Nafría <albert.nafria@gmail.com>
2023-10-06 20:37:46 +0200

    list comprehension in loop

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ screen.title("Spanish Provinces Game")
 
 image = "spanish_map.gif"
 provinces_csv_file = "provinces.csv"
+provinces_missing_csv_file = "provinces_missing.csv"
 
 guessed_provinces = []
 
@@ -26,13 +27,10 @@ while len(guessed_provinces) < len(provinces_list):
     answer = screen.textinput(title=f"{len(guessed_provinces)}/{len(provinces_list)} provinces correct",
                               prompt="Tell me another province:").title()
     if answer == "Exit":
-        missing_provinces = []
-        for province in provinces_list:
-            if province not in guessed_provinces:
-                missing_provinces.append(province)
+        missing_provinces = [province for province in provinces_list if province not in guessed_provinces]
         # Save a list of missing provinces
         missing_provinces_data = pandas.DataFrame(missing_provinces)
-        missing_provinces_data.to_csv("provinces_missing.csv")
+        missing_provinces_data.to_csv(provinces_missing_csv_file)
         break
 
     if answer in provinces_list:
